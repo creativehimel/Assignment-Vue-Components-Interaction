0dc43e51f975ddad53204102957b6a23c6cbc3a5
Md. Himel Ali <himel.phy160@gmail.com>
2023-08-20 22:08:02 +0600

    Assignment Done

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps(['modelValue'])
+function getCustomMessage(){
+  return 'Custom Message from Child Component!'
+}
+</script>
+
+<template>
+<section class="my-16">
+  <h2 class="text-3xl font-bold text-red-500 my-5 text-center border-y-2 py-3 border-red-300">Child Component</h2>
+  <p class="text-lg font-semibold text-center text-gray-500" >{{ modelValue }}</p>
+  <div class="flex items-center justify-center my-5">
+    <button class="bg-red-500 px-6 py-2 rounded text-white text-center" @click="$emit('update:modelValue', getCustomMessage())">Custom Message</button>
+  </div>
+
+</section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue";
+import ChildComponent from "@/components/ChildComponent.vue";
+const msg = ref('Hi I am a Vue Learner!')
+</script>
+
+<template>
+<section class="mx-auto container my-10">
+  <h2 class="text-3xl font-bold text-green-500 my-5 text-center border-y-2 py-3 border-green-300">Parent Component</h2>
+  <p class="text-lg font-semibold text-center text-gray-500" >{{ msg }}</p>
+  <ChildComponent v-model="msg"/>
+</section>
+</template>
+
+<style scoped>
+
+</style>
